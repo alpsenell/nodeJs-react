e771ddd309a212ec0ed5cb6d716b009dc6089a74
alpsenell <alp@useinsider.com>
2020-06-21 22:30:16 +0300

    Start react side

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+<h2>Hello React from JSX { Math.random().toFixed(2) }</h2>,
+    document.getElementById('root')
+);
